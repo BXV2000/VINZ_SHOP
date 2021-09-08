8e840ff993bb9ad8fc8e607b092cb1b971d1fada
RUKICUA <bxv.se.gd@gmail.com>
2021-09-08 21:14:44 +0700

    edit some calculation

--- cart.php
+++ cart.php
@@ -59,13 +59,13 @@ if(isset($_POST['remove'])){
                         if($row['id_sp'] == $id){
                             ?>
                             <form action="cart.php?action=remove&id=<?php echo $row['id_sp']; ?>" method="post" class="item">
-                            <input type="hidden" class="ten_sp_cart" value="<?php echo $row['ten_sp']; ?>">
-                            <input type="hidden" class="id_sp_cart" value="<?php echo $row['id_sp']; ?>">  
+                            <input type="hidden" class="cart_product_id" value="<?php echo $row['id_sp']; ?>">  
+                            <input type="hidden" class="cart_product_price" value="<?php echo $row['gia_sp']; ?>">  
                             <tr>
                                 <td class="delete_column"><button type="submit" name='remove' class='item_remove'><i class="fas fa-times"></i></button></td>
                                 <td class="img_column"><img src="img/<?php echo $row['hinh_anh']; ?>" alt="" class="item_img"></td>
                                 <td class="name_column"><p class="item_name"><?php echo $row['ten_sp']; ?></p></td>
-                                <td class="price_column"><p class="item_price"><?php echo $row['gia_sp']; ?></p></td>
+                                <td class="price_column"><p class="item_price"><?php echo $row['gia_sp']; ?> VNĐ</p></td>
                                 <td class="quantity_column"><input type="number" class='item_quantity' name='quantity' value='1' ></td>
                                 <td class="total_column"><input type="text" class='item_total' value="" disabled></td>
                             </tr>   
@@ -87,62 +87,60 @@ if(isset($_POST['remove'])){
         </div>
     </div>
     <script>
-       
-        
-        function themHoaDon(){
-            let items = document.getElementsByClassName('item');
-            let id_items = document.getElementsByClassName('id_sp_cart');
-            let name_items = document.getElementsByClassName('ten_sp_cart');
-            let quantity_items =document.getElementsByClassName('quantity');
-            document.getElementById("hoa_don").innerHTML += `<input type="hidden" name="item_number" value="${items.length}">`;
-            for(let i = 0;i<items.length;i++){
-            document.getElementById("hoa_don").innerHTML += 
-              `<input type="hidden" name="id_sp_${i}" value="${id_items[i].value}">
-                <input type="hidden" name="ten_sp_${i}" value="${name_items[i].value}">
-                <input type="hidden" name="soluong_${i}" class="checkout_quantity" value="${quantity_items[i].value}">`;
-            }
+        let itemQuantityList = [];
+
+        function firstLoadCal(){
+            let itemList = document.getElementsByClassName("cart_product_id");
+            console.log(itemList.length);
         }
-        
-        function kt_so_luong(ds_so_luong){
-            let items = document.getElementsByClassName('item');
-            if (ds_so_luong.length===0){
-                let items = document.getElementsByClassName('item');
-                for(let i = 0;i<items.length;i++){
-                    ds_so_luong.push(1);
+
+        function getItemQuantity(){
+            itemList = document.getElementsByClassName("cart_product_id");
+            if(getItemQuantity.length===0){
+                for(let i = 0; i<itemList.length;i++){
+                    itemQuantityList.push(1);
                 }
-                
             }
         }
 
-        function tinhTienBanDau(){
-            let tong_tien=0;
-            let item_price = document.getElementsByClassName('item_price');
-            for(let i = 0;i<ds_so_luong.length;i++){
-                tong_tien+= parseInt(item_price[i].innerHTML)*ds_so_luong[i];
-            }
-            document.getElementById('total_cost').value=tong_tien;
-            console.log(document.getElementById('total_cost').value)
-        }
-
-        function tinhTien(){
-            let items = document.getElementsByClassName('item');
-            let quantity =document.getElementsByClassName('quantity');
-            let item_price = document.getElementsByClassName('item_price');
-            let item_quantity = document.getElementsByClassName('checkout_quantity');
-            for(let i = 0;i<items.length;i++){
-                quantity[i].addEventListener("change", function () {
-                    ds_so_luong[i]=parseInt(quantity[i].value);
-                    tinhTienBanDau();
-                    item_quantity[i].value=quantity[i].value;
-                    console.log(item_quantity[i].value)
+        function quantityUpdate(){
+            let itemList = document.getElementsByClassName("cart_product_id");
+            let itemQuantity = document.getElementsByClassName("item_quantity");
+            for(let i = 0; i<itemList.length;i++){
+                itemQuantity[i].addEventListener('change', () => {
+                    itemQuantityList[i]=parseInt(itemQuantity[i].value);
+                    billTotal();
+                    itemTotal();
                 });
             }
         }
-        let ds_so_luong=[];
-        kt_so_luong(ds_so_luong);
-        tinhTienBanDau();
-        tinhTien();
-        themHoaDon();
+
+        function billTotal(){
+            let itemList = document.getElementsByClassName("cart_product_id");
+            let itemPrices = document.getElementsByClassName("cart_product_price");
+            let billTotal=0;
+            for(let i = 0; i<itemList.length;i++){
+                billTotal += parseInt(itemPrices[i].value) *  itemQuantityList[i];
+            }
+            document.getElementById("total_cost").value = billTotal;
+        }
+
+        function itemTotal(){
+            let itemList = document.getElementsByClassName("cart_product_id");
+            let itemPrices = document.getElementsByClassName("cart_product_price");
+            let itemTotal = document.getElementsByClassName("item_total");
+            for(let i = 0; i<itemList.length;i++){
+                itemTotal[i].value = parseInt(itemPrices[i].value) *  itemQuantityList[i];
+            }
+
+        }
+
+
+       firstLoadCal();
+       getItemQuantity();
+       billTotal();
+       itemTotal();
+       quantityUpdate();
     </script>
 </body>
 </html>
